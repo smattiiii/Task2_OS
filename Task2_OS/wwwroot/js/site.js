0cf582d736f18815db76c661ab75df5ce35ce611
sofiamattinzioli <334739@edu.mkcollege.ac.uk>
2025-02-11 22:11:09 +0000

    login Partial

--- Task2_OS/wwwroot/js/site.js
+++ Task2_OS/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#navigation-explore').each(function () {
+        var $toolip = $(this).Tooltip({
+            html: true,
+            title: $('#explore-title').html(),
+            trigger: 'manual',
+        }).on({
+            mouseenter: function () {
+                if ($('.tooltip').length == 0) //prevent tooltop from hiding when explore is hovered oer
+                    $(this).tooltip('show'); //show tooltip over mouseenter
+            },
+            click: function (event) {
+                if (event.pointerType === "touch") {
+                    //ignore click event for touch
+                    //explore button twice to work, due to  touch event
+                    // calls both mouseenter and click one after the other 
+                } else
+                    $(this).tooltip('show'); //toggle tooltip on click
+            }
+        });
+        
+        $(document).on('click', function (event) {
+            var search = $('.tooltip');
+            if (search.length == 0) //tooltip doesn't exist 
+                return;
+            var $target = $(event.target);
+            var isChildOfToolTip = search[0].contains($target[0]);
+            if (!$target.is('#navigation-explore') && !isChildOfToolTip) {
+                $tooltip.Tooltip('hide');
+            }
+
+        });
+    });
+});
